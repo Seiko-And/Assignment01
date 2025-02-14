b58a405ac09b0148f0b280b5f46d85df67e8261d
Seiko-And <seiko.a125@gmail.com>
2025-02-14 11:39:18 -0800

    created a new folder and new files. added Javascript and html codes.

---
+++ js/script.js
@@ -0,0 +1,133 @@
+// STEP 1
+// let someMonth;
+// function theMonth(){};
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction
+
+// STEP 2
+// 2.5               // Numeric literal
+// 'Hello world'     // String literal
+// true             // Boolean literal
+// null             // Null literal
+
+// STEP 3
+// let sum = 5 + 6;
+// let message = alert('Hello!!');
+
+// Testing
+// console.log(sum);
+
+// STEP 4
+// let strFirstName
+// let lastName;
+// let address;
+// let city;
+// let state;
+// let intZipCode;
+// let intYourAge;
+// let refferalSource;
+// let blnMayWeContactYou;
+
+// STEP 5
+// let firstName;
+// firstName ='Seiko';
+// const lastName = 'Anderson';
+// var address = '123 NE St', city = 'San Diego';
+
+// Testing
+// console.log(firstName);
+// console.log(lastName);
+// console.log(address, city);
+
+// STEP 6
+// const zipCode = 92111;
+// console.log(zipCode);
+// const firstName = 'Seiko';
+// console.log(firstName);
+
+// STEP 7
+// const result1 = true + ' is the result';
+// console.log(result1);
+// const result2 = true + 11;
+// console.log(result2);
+
+// STEP 8
+/* The following string literal in NOT valid.
+   I would add \(escape sequence) before double and single quotation marks. */
+// let someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"'
+// console.log(someString);
+
+// STEP 9
+// const num = null;
+// console.log(num);
+// let x;
+// console.log(x);
+
+// STEP 10
+// console.log(typeof 'hello');
+// console.log(typeof 11);
+// console.log(typeof true);
+// console.log(typeof window);
+// console.log(typeof x);
+
+// STEP 11
+// alert('Hello ' + 'Seiko Anderson' + ' , welcome to the JavaScript class!');
+
+// STEP 12
+// let myName;
+// myName = 'Seiko Anderson';
+// alert('Hello ' + myName + ' , welcome to the JavaScript class!');
+
+// STEP 13
+// let myName, course;
+// myName = 'Seiko Anderson' 
+// course = 'JavaScript';
+// alert('Hello ' + myName + ' , welcome to the ' + course + ' class!');
+
+// STEP 14
+// let myName, course;
+// myName = 'Seiko Anderson' 
+// course = 'JavaScript';
+// alert('Hello ' + myName  + '.\nWelcome to the ' + course + ' class!');
+
+// STEP 15
+// const userName = prompt('Enter your name.');
+// const course = 'JavaScript';
+// alert('Hello ' + userName  + '.\nWelcome to the ' + course + ' class!');
+
+// STEP 16
+// const userName = prompt('Enter your name.'); 
+// const course = prompt('Enter the class that you are taking.');
+// alert('Hello ' + userName  + '.\nWelcome to the ' + course + ' class!');
+
+// STEP 17
+// const x = 10;
+// const y = 20;
+// const sum = x + y;
+// console.log(sum);
+
+// STEP 18
+// let x = 20;
+// x += 20;
+// console.log(x);
+
+// STEP 19
+// let x = 20;
+// x *= 5;
+// console.log(x);
+
+// STEP 20
+// let x = 20%3;
+// x /= 1;
+// console.log(x);
+
+// STEP 21
+// const x = 100;
+// const y = 50;
+// console.log(x >= 100 || y > 50);
+
+// STEP 22
+// const x = 100;
+// const y = '100';
+// console.log(x === 100 && y === 100);
